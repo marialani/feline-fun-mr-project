9e44ffb07fce20e1690978014a9a34ed55887f54
Maria <m.olanipekun@hotmail.com>
2020-01-19 10:43:54 +0000

    create vet button with state, update css style and include disabled visual for each button

--- src/components/EnergyMetre.js
+++ src/components/EnergyMetre.js
@@ -17,12 +17,13 @@ const EnergyMetre = ({catEnergy,setCatEnergy,selectedCat}) => {
 
     const [disableFeed, setDisableFeed] = React.useState(false)
     const [disablePlay, setDisablePlay] = React.useState(false)
+    const [disableVet, setDisableVet] = React.useState(false)
 
     return (
         <div>
           <progress max="5" value={`${catEnergy}`}></progress>
           <div className="btnFlex">
-              <button className="feedBtn" disabled={disableFeed} onClick={() => {
+            <button className="feedBtn" disabled={disableFeed} onClick={() => {
                 
               setCatEnergy(prevCatEnergy => {
                 if (catEnergy === 5) {
@@ -35,19 +36,29 @@ const EnergyMetre = ({catEnergy,setCatEnergy,selectedCat}) => {
                 return prevCatEnergy +1
             })
             }}>FEED</button>
-    
+          <button className="vetBtn" disabled={disableVet} onClick={() => {
+                setCatEnergy(prevCatEnergy => {
+                if (catEnergy === 5) {
+                    setDisableVet(!disableVet)
+                    setTimeout(() => {
+                        setDisableVet(disableVet)},3000)
+                    }
+                else if(prevCatEnergy>0 && prevCatEnergy < 5) 
+                setDisableVet(!disableVet)
+                setTimeout(() => {setDisableVet(disableVet)},2500)
+                return prevCatEnergy +1
+          })
+          }}>VET</button>
           <button className="playBtn" disabled ={disablePlay} onClick={() => {
                 setCatEnergy(prevCatEnergy => {
                 if (catEnergy === 5) {
                     setDisablePlay(!disablePlay)
                     setTimeout(() => {
-                        setDisablePlay(disablePlay);
-                        // document.body.classList.playBtn.add('greyed');
-                    },3000)
+                        setDisablePlay(disablePlay)},3000)
                     }
                 else if(prevCatEnergy>0 && prevCatEnergy < 5) 
                 setDisablePlay(!disablePlay)
-                setTimeout(() => {setDisablePlay(disablePlay)},1000)
+                setTimeout(() => {setDisablePlay(disablePlay)},2000)
                 return prevCatEnergy +1
           })
           }}>PLAY</button>
